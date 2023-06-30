8fc43bf198f00f723daf0df94f263a24ed41d5b6
ArtaXerxes <74452252+ArtaXerxess@users.noreply.github.com>
2023-06-30 12:17:56 +0530

    Initial commit

---
+++ bookscraper/notes.md
@@ -0,0 +1,59 @@
+# Notes
+
+Target: `books.toscrape.com`
+
+Concepts: [`css selectors, xpath`](https://docs.scrapy.org/en/latest/topics/selectors.html) 
+
+Using ipython shell , and venv as recommended in doccumentation
+
+Create spider:
+
+>scrapy genspider bookspider books.toscrape.com
+
+Write Code : [Spider.py](spiders\bookspider.py)
+
+Crawl and Save output:
+
+>scrapy crawl bookspider -o books.csv
+
+Maybe Scrapy is ez... 
+
+### Using Scrapy Shell
+
+>fetch('https://books.toscrape.com')
+
+>response
+
+>response.css('article.product_pod')
+
+>books = response.css('article.product_pod')
+
+>book = books[0] # first book
+
+>book.css('h3 a::text').get()
+
+>book.css('.product_price .product_color::text').get()
+
+>book.css('h3 a').attrib['href'] # href attribute, link to the book
+
+>response.css('li.next a ::attr(href)').get()
+
+#### Product page
+
+on inspect element one can also use  right_click > copy > copy Selector
+
+>fetch('https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html')
+
+>response.css('.product_page h1::text').get()
+
+>response.css('.product_page p::text').get() # price
+
+>response.css('.product_page p::text').getall()[-1] # product's detailed description
+
+or
+
+>response.css('#content_inner > article > p::text').get() product's detailed description
+
+>response.css('#default > div > div > ul > li:nth-child(3) > a::text').get()  
+
+>response.css('#content_inner > article > div.row > div.col-sm-6.product_main > p.star-rating::attr(class)').get()
---
+++ bookscraper/spiders/bookspider.py
@@ -0,0 +1,38 @@
+import scrapy
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com"]
+    def parse(self, response):
+        books = response.css("article.product_pod")
+        for book in books:
+            relative_url = book.css("h3 a ::attr(href)").get()
+            if "catalogue/" in relative_url:
+                book_url = "https://books.toscrape.com/" + relative_url
+            else:
+                book_url = "https://books.toscrape.com/catalogue/" + relative_url
+            yield response.follow(book_url, callback=self.parse_book_page)
+        next_page = response.css("li.next a ::attr(href)").get()
+        if next_page is not None:
+            if "catalogue/" in next_page:
+                next_page_url = "https://books.toscrape.com/" + next_page
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+            yield response.follow(next_page_url, callback=self.parse)
+    def parse_book_page(self, response):
+        table_rows = response.css("table tr")
+        yield {
+            "Title": response.css(".product_main h1::text").get(),
+            "Price excl tax": table_rows[2].css("td ::text").get(),
+            "Price incl tax": table_rows[3].css("td ::text").get(),
+            "Tax": table_rows[4].css("td ::text").get(),
+            "Availability": table_rows[5].css("td ::text").get(),
+            "Stars": response.css("p.star-rating").attrib["class"],
+            "Category": response.css(
+                "#default > div > div > ul > li:nth-child(3) > a::text"
+            ).get(),
+            "Product description": response.css(
+                "#content_inner > article > p::text"
+            ).get(),
+            "Price": response.css("p.price_color ::text").get(),
+        }
